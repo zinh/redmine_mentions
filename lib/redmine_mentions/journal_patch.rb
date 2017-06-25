380c00956be164d6f91224d9c1b0a46075ad20f4
Vinh Nguyen <xuanvinhct@gmail.com>
2017-06-25 14:13:09 +0700

    fix mailer

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -9,13 +9,13 @@ module RedmineMentions
             issue = self.journalized
             project=self.journalized.project
             users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
-            users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
+            users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.firstname}\/#{u.id}"}.join('|')
             regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
             mentioned_users = self.notes.scan(regex)
             mentioned_users.each do |mentioned_user|
-              username = mentioned_user.first[1..-1]
-              if user = User.find_by_login(username)
+              user_id = mentioned_user.first[/\/\d+$/].delete('/')
+              if user = User.find_by_id(user_id)
                 MentionMailer.notify_mentioning(issue, self, user).deliver
               end
             end
