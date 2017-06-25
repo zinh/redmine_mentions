4f1fa59d990ddbe6d192afa94a08f8ef1cddf660
Vinh Nguyen <xuanvinhct@gmail.com>
2017-06-25 14:17:43 +0700

    version 0.0.3

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_mentions do
   name 'Redmine Mentions'
   author 'Arkhitech'
   description 'This is a plugin for Redmine which gives suggestions on using username in comments'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/arkhitech/redmine_mentions'
   author_url 'http://www.arkhitech.com/'
   settings :default => {'trigger' => '@'}, :partial => 'settings/mention'
